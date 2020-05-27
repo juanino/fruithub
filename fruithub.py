42cb3f66ac7bddb0846e9f8043322d079234d8ac
Cloud Jerry <juanino@gmail.com>
2020-05-27 19:54:45 -0400

    sms for sensor change via aws. helper script for starting flask

--- fruithub.py
+++ fruithub.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
+import sys
+try:
+    import config as cfg
+except:
+    print("could not find config file")
+    print("copy config_sample.py to config.py and adjust")
+    sys.exit(1)
+
 from markupsafe import escape
 
 from flask import Flask
 app = Flask(__name__)
+
+import boto3
+client = boto3.client('sns')
 
 @app.route('/')
 def hello_world():
@@ -15,4 +26,7 @@ def checkin(devicename):
 
 @app.route('/post/<devicename>/<sensor>/<status>')
 def store_sensor(devicename,sensor,status):
-    return 'fruithub: Device:' + devicename + ' sensor:' + sensor + ' has status:' + str(status)
+    # Publish a message.
+    msg ='fruithub: Device:' + devicename + ' sensor:' + sensor + ' has status:' + str(status) 
+    client.publish(Message=msg, TopicArn=cfg.topic_arn)
+    return msg
