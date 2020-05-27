cfa4f0213827e27b44d0a9942d49bbebd004958a
Cloud Jerry <juanino@users.noreply.github.com>
2020-05-27 19:59:13 -0400

    Delete send_sms.py

--- send_sms.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import boto3
-
-client = boto3.client('sns')
-
-# Publish a message.
-client.publish(Message="Good news everyone!",
-                 TopicArn="arn:aws:sns:us-east-1:292915697840:waterpi")
